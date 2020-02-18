e3da19217bcb860f75a0daa2b93b9b7933266e2a
Hector Jaramillo <43076992+HectorJaramillo@users.noreply.github.com>
2020-02-18 14:48:03 -0800

    Create HolamMundo.s

---
+++ HolamMundo.s
@@ -0,0 +1,14 @@
+.data 
+cadena: .asciz "Hola  Mundo!\n"
+cadenafin:
+.text
+.global  main
+main:    PUSH     {r7, lr} 
+        MOV      r0, #1             
+        LDR    r1, =cadena    
+        MOV      r2, #cadenafin -cadena      
+        MOV      r7, #4  
+        SWI     #0        
+        MOV     r0, #0  
+        POP     {r7, lr}          
+        BX      lr   
